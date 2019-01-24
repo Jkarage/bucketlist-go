8f6f0fde6f6b873cb5fdd4798e6af4f359fc61fe
David Mukiibi <david.mukiibiq@gmail.com>
2019-01-24 16:19:29 +0300

    made a few changes

--- setup-go.sh
+++ setup-go.sh
@@ -5,7 +5,11 @@ set -o pipefail
 
 install_go() {
     wget -c https://storage.googleapis.com/golang/go1.11.2.linux-amd64.tar.gz
-    tar -C /usr/local -xzf go1.11.2.linux-amd64.tar.gz
+    sudo chown $USER: -R /usr/local
+    sudo chmod u+w /usr/local
+    sudo chown $USER: -R go/
+    sudo chmod u+w go/
+    tar -C /usr/local -xvzf go1.11.2.linux-amd64.tar.gz
     export PATH=$PATH:/usr/local/go/bin
     source $HOME/.profile
 }
