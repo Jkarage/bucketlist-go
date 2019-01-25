3e80e09aeea69cc3140aa18ee84d8e08a2275b4a
David Mukiibi <david.mukiibiq@gmail.com>
2019-01-25 11:49:10 +0300

    Still adding debug pointers

--- setup-go.sh
+++ setup-go.sh
@@ -5,22 +5,33 @@ set -o pipefail
 
 install_go() {
     wget -c https://storage.googleapis.com/golang/go1.11.2.linux-amd64.tar.gz
-    echo "this is done 1"
     sudo chown $USER: -R /usr/local
-    echo "this is done 2"
     sudo chmod u+w /usr/local
-    echo "this is done 3"
     tar -C /usr/local -xvzf go1.11.2.linux-amd64.tar.gz
-    echo "this is done 4"
     export PATH=$PATH:/usr/local/go/bin
-    echo "this is done 5"
     source $HOME/.profile
+    echo "go installation done..."
+}
+
+setup_go() {
+    # go install controllers routes services
+    go get -u github.com/gorilla/handlers
+    echo "this is done 1"
+    go get -u github.com/auth0/go-jwt-middleware
+    echo "this is done 2"
+	go get -u github.com/dgrijalva/jwt-go
+    echo "this is done 3"
+	go get -u github.com/gorilla/mux
+    echo "this is done 4"
+	go get -u github.com/jinzhu/gorm
+    echo "this is done 5"
+	go get -u github.com/lib/pq
     echo "this is done 6"
-    go install controllers routes services
+    go get -u github.com/joho/godotenv
     echo "this is done 7"
-    go get -u golang.org/x
+    go get -u github.com/jinzhu/gorm/dialects/postgres
     echo "this is done 8"
-    go get -u github.com
+	go get -u golang.org/x/crypto/bcrypt
     echo "this is done 9"
 }
 
