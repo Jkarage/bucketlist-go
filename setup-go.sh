4daae0546ee6c923f5637dc8d6564663cb17a040
David Mukiibi <david.mukiibiq@gmail.com>
2019-01-25 11:36:05 +0300

    Added debugging messages

--- setup-go.sh
+++ setup-go.sh
@@ -5,11 +5,17 @@ set -o pipefail
 
 install_go() {
     wget -c https://storage.googleapis.com/golang/go1.11.2.linux-amd64.tar.gz
+    echo "this is done 1"
     sudo chown $USER: -R /usr/local
+    echo "this is done 2"
     sudo chmod u+w /usr/local
+    echo "this is done 3"
     tar -C /usr/local -xvzf go1.11.2.linux-amd64.tar.gz
+    echo "this is done 4"
     export PATH=$PATH:/usr/local/go/bin
+    echo "this is done 5"
     source $HOME/.profile
+    echo "this is done 6"
     go install controllers routes services
     go get -u golang.org/x
     go get -u github.com
