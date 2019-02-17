5d1d22c608908c98899150bb775111a1ffa28a92
David Mukiibi <david.mukiibiq@gmail.com>
2019-02-18 00:58:57 +0300

    Dummy env variables

---
+++ env.sh
@@ -0,0 +1,8 @@
+export GOPATH=~/workspace/bucketlist-go
+export DB_HOST=localhost
+export DB_DATABASE=kenya
+export DB_USER=postgres
+export DB_PASS=secretsecretsecret
+export SSL_MODE=disable
+export DB_TYPE=postgres
+export SECRET=some_random_secret_is_not_a_secret
