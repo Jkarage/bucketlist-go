9e8a73c7cbdf151db907c09d5c908548202d6965
David Mukiibi <david.mukiibiq@gmail.com>
2019-03-11 00:34:29 +0300

    commented out go installation steps

--- cloud-deployment/AWS/packer/setup-go.sh
+++ cloud-deployment/AWS/packer/setup-go.sh
@@ -4,10 +4,10 @@ set -e
 set -o pipefail
 
 install_go() {
-    wget -c https://storage.googleapis.com/golang/go1.11.2.linux-amd64.tar.gz
-    sudo chown $USER: -R /usr/local
-    sudo chmod u+w /usr/local
-    tar -C /usr/local -xvzf go1.11.2.linux-amd64.tar.gz
+        # wget -c https://storage.googleapis.com/golang/go1.11.2.linux-amd64.tar.gz
+        # sudo chown $USER: -R /usr/local
+        # sudo chmod u+w /usr/local
+        # tar -C /usr/local -xvzf go1.11.2.linux-amd64.tar.gz
     export PATH=$PATH:/usr/local/go/bin
     source $HOME/.profile
     echo "Go Installation & Setup Completed Successfully..."
