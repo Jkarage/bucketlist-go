0959c4a6ebf94fd61d5967c275daf9ac03d5c01c
David Mukiibi <david.mukiibiq@gmail.com>
2019-03-11 09:00:41 +0300

    made a few changes to install postgres too

--- cloud-deployment/AWS/packer/setup-ansible.sh
+++ cloud-deployment/AWS/packer/setup-ansible.sh
@@ -7,7 +7,7 @@ setup_ansible() {
 	sudo apt-get install -y software-properties-common
     sudo apt-add-repository ppa:ansible/ansible
     sudo apt-get update
-    sudo apt-get install -y ansible
+    sudo apt-get install -y ansible postgresql postgresql-contrib
 }
 
 main() {
