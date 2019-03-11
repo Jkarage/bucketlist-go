eb88405bfee003a4bbf3ccb81657d35941a2372c
David Mukiibi <david.mukiibiq@gmail.com>
2019-03-11 09:10:04 +0300

    fixing missing package issues

--- cloud-deployment/AWS/packer/setup-ansible.sh
+++ cloud-deployment/AWS/packer/setup-ansible.sh
@@ -4,7 +4,8 @@ set -e
 set -o pipefail
 
 setup_ansible() {
-	sudo apt-get install -y software-properties-common
+	sudo apt-get update
+    sudo apt-get install -y software-properties-common
     sudo apt-add-repository ppa:ansible/ansible
     sudo apt-get update
     sudo apt-get install -y ansible postgresql postgresql-contrib
