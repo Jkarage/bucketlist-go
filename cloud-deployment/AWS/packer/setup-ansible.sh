b7c5711832f17622bfae7ea8ac23727b213f8a55
David Mukiibi <david.mukiibiq@gmail.com>
2019-03-11 09:29:05 +0300

    made a few changes

--- cloud-deployment/AWS/packer/setup-ansible.sh
+++ cloud-deployment/AWS/packer/setup-ansible.sh
@@ -4,10 +4,11 @@ set -e
 set -o pipefail
 
 setup_ansible() {
-    sudo rm /var/lib/apt/lists/lock
-    sudo rm /var/cache/apt/archives/lock
-    sudo rm /var/lib/dpkg/lock
-    sudo dpkg --configure -a
+    # sudo rm /var/lib/apt/lists/lock
+    # sudo rm /var/cache/apt/archives/lock
+    # sudo rm /var/lib/dpkg/lock
+    # sudo dpkg --configure -a
+    sudo apt-get clean
 	sudo apt-get update
     sudo apt-get install -y postgresql postgresql-contrib
 }
