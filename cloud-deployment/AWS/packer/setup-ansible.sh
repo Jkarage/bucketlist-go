96537559c6bfcde085bc69588b4f9b2b5896ea5b
David Mukiibi <david.mukiibiq@gmail.com>
2019-03-11 09:21:39 +0300

    ng lock file issues

--- cloud-deployment/AWS/packer/setup-ansible.sh
+++ cloud-deployment/AWS/packer/setup-ansible.sh
@@ -4,7 +4,10 @@ set -e
 set -o pipefail
 
 setup_ansible() {
-    sudo killall apt apt-get
+    sudo rm /var/lib/apt/lists/lock
+    sudo rm /var/cache/apt/archives/lock
+    sudo rm /var/lib/dpkg/lock
+    sudo dpkg --configure -a
 	sudo apt-get update
     sudo apt-get install -y postgresql postgresql-contrib
 }
