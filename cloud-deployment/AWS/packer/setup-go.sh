1138fa5c44e3bc62ad4433f75a69057615833449
David Mukiibi <david.mukiibiq@gmail.com>
2019-03-11 10:03:17 +0300

    commented out some commands

--- cloud-deployment/AWS/packer/setup-go.sh
+++ cloud-deployment/AWS/packer/setup-go.sh
@@ -3,9 +3,9 @@
 set -e
 set -o pipefail
 
-setup_ansible() {
-    sudo pkill apt-get
-    sudo pkill apt
+setup_postgres() {
+    # sudo pkill apt-get
+    # sudo pkill apt
 	sudo apt-get update
     sudo apt-get install -y postgresql postgresql-contrib
 }
