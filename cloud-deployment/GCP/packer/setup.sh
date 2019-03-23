2226b10444c117737d8e468887d461bca960e792
David Mukiibi <david.mukiibiq@gmail.com>
2019-03-23 20:10:48 +0300

    added this too

---
+++ cloud-deployment/GCP/packer/setup.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+install_go() {
+    # sudo su ubuntu && \
+    cd ~/bucketlist-go && wget -c https://storage.googleapis.com/golang/go1.11.2.linux-amd64.tar.gz && \
+    sudo chown $USER: -R /usr/local && \
+    sudo chmod u+w /usr/local && \
+    tar -C /usr/local -xvzf go1.11.2.linux-amd64.tar.gz && \
+    export PATH=$PATH:/usr/local/go/bin && \
+    source $HOME/.profile && \
+    sudo rm -rf go1.11.2.linux-amd64.tar.gz && \
+    echo "Go Installation & Setup Completed Successfully..."
+}
+
+setup_go_app() {
+    echo "exporting GOPATH..." && \
+    export GOPATH=~/bucketlist-go && \
+    echo "exported GOPATH..." && \
+    echo "Downloading and installing supporting Go packages..." && \
+    cd ~/bucketlist-go && go get -u github.com/gorilla/handlers \
+    github.com/auth0/go-jwt-middleware \
+    github.com/dgrijalva/jwt-go \
+    github.com/gorilla/mux \
+    github.com/jinzhu/gorm \
+    github.com/lib/pq \
+    github.com/joho/godotenv \
+    github.com/jinzhu/gorm/dialects/postgres \
+    golang.org/x/crypto/bcrypt \
+    github.com/davidmukiibi/controllers \
+    github.com/davidmukiibi/routes \
+    github.com/davidmukiibi/services
+    echo "Downloading and installing supporting Go packages finished..."
+}
+
+install_postgres() {
+    sudo apt-get update && \
+    sudo apt-get install -y postgresql \
+    postgresql-contrib \
+    postgresql-client
+}
+
+set_up_postgres() {
+    cp createdb.sql /docker-entrypoint-initdb.d
+}
+
+main() {
+    install_go
+    setup_go_app
+    install_postgres
+    set_up_postgres
+}
+
+main "$@"
