8679677d8e4774cf0712e48d161318ca2e3fd606
David Mukiibi <david.mukiibiq@gmail.com>
2019-03-25 11:11:15 +0300

    made a few tweaks here and there

--- cloud-deployment/GCP/packer/setup.sh
+++ cloud-deployment/GCP/packer/setup.sh
@@ -4,52 +4,19 @@ set -e
 set -o pipefail
 
 install_go() {
-    sudo su ubuntu && \
-    cd ~/bucketlist-go && wget -c https://storage.googleapis.com/golang/go1.11.2.linux-amd64.tar.gz && \
-    sudo chown $USER: -R /usr/local && \
-    sudo chmod u+w /usr/local && \
-    tar -C /usr/local -xvzf go1.11.2.linux-amd64.tar.gz && \
-    export PATH=$PATH:/usr/local/go/bin && \
-    source $HOME/.profile && \
-    sudo rm -rf go1.11.2.linux-amd64.tar.gz && \
-    echo "Go Installation & Setup Completed Successfully..."
-}
-
-setup_go_app() {
-    echo "exporting GOPATH..." && \
-    export GOPATH=~/bucketlist-go && \
-    echo "exported GOPATH..." && \
-    echo "Downloading and installing supporting Go packages..." && \
-    cd ~/bucketlist-go && go get -u github.com/gorilla/handlers \
-    github.com/auth0/go-jwt-middleware \
-    github.com/dgrijalva/jwt-go \
-    github.com/gorilla/mux \
-    github.com/jinzhu/gorm \
-    github.com/lib/pq \
-    github.com/joho/godotenv \
-    github.com/jinzhu/gorm/dialects/postgres \
-    golang.org/x/crypto/bcrypt \
-    github.com/davidmukiibi/controllers \
-    github.com/davidmukiibi/routes \
-    github.com/davidmukiibi/services
-    echo "Downloading and installing supporting Go packages finished..."
-}
-
-setting_env_vars() {
-    export DB_HOST=localhost && \
-    export DB_DATABASE=kenya && \
-    export DB_USER=postgres && \
-    export DB_PASS=secretsecretsecret && \
-    export SSL_MODE=disable && \
-    export DB_TYPE=postgres && \
-    export SECRET=some_random_secret_is_not_a_secret
+	cd /tmp && wget -c https://storage.googleapis.com/golang/go1.11.2.linux-amd64.tar.gz && \
+	sudo chown $USER: -R /usr/local && \
+	sudo chmod u+w /usr/local && \
+	tar -C /usr/local -xvzf go1.11.2.linux-amd64.tar.gz && \
+	echo "Go Installation & Setup Completed Successfully..."
 }
 
 install_postgres() {
     sudo apt-get update && \
     sudo apt-get install -y postgresql \
     postgresql-contrib \
-    postgresql-client
+    postgresql-client && \
+    echo "installing postgres server and client completed..."
 }
 
 set_up_postgres() {
@@ -62,10 +29,8 @@ EOF
 }
 
 main() {
-    # setting_env_vars
-    # install_go
-    # setup_go_app
-    # install_postgres
+    install_go
+    install_postgres
     set_up_postgres
 }
 
